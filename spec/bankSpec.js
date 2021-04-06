63a8b9a6781a7c14780ef891d92262ae07cb4a8a
Christine Chee <christinechee88@hotmail.com>
2021-04-06 14:49:59 +0100

    Add newAccount method for Bank class to create new account and unit test

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -2,13 +2,23 @@ describe('Bank', function() {
   var Bank = require('../lib/Bank');
   var bank; 
 
-  beforeEach(() => {
-		bank = new Bank;
-	});
+  
+  beforeEach(function() {
+    let account = jasmine.createSpy('account');
+    bank = new Bank(account);
+  });
     
   it('should instantiate a new Bank instance', () => {   
     expect(bank).toBeInstanceOf(Bank);
   });
-  
+
+  describe('newAccount', function() {
+    it('creates a new account', () => {
+      account = jasmine.createSpy('account');
+      bank = new Bank(account);
+      bank.newAccount();
+      expect(account).toHaveBeenCalled();
+    });    
+  });   
 });
   
