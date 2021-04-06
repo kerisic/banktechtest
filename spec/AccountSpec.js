47348f89a4cbfef959a71ab0bee1682035e58df8
Christine Chee <christinechee88@hotmail.com>
2021-04-06 20:04:32 +0100

    Add withdraw function for Account class

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -1,6 +1,6 @@
 describe('Account', function() {
-  var Account = require('../lib/Account');
   var account;
+  var Account = require('../lib/Account');
 
   beforeEach(() => {
 		account = new Account;
@@ -13,5 +13,30 @@ describe('Account', function() {
   it('should start with a balance of zero', () => {
     expect(account.balance).toEqual(0);
   });
+
+  describe('deposit()', function() {
+    it('should add funds into the account and increase the balance by the amount', () => {
+      let amount = 200;
+      expect(account.balance).toEqual(0)
+      account.deposit(amount);
+      expect(account.balance).toEqual(amount);
+    });
+
+    it('should ensure balance increases by right amount when balance is not zero', () => {
+      let amount = 200;
+      account.deposit(amount);
+      account.deposit(amount);
+      expect(account.balance).toEqual(400);
+    });
+  });
+  
+  describe('withdraw()', function() {
+    it('should take out funds from the account and decrease the balance by the amount', () => {
+      account.deposit(400);
+      account.withdraw(200);
+      expect(account.balance).toEqual(200);
+    });
+  });
+        
 });
   
